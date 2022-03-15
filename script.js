1be8a6f8c814ff0550b55c1b58f85af11208593c
N11rm44L 7w44711 <60023424+niirmaaltwaatii@users.noreply.github.com>
2022-03-15 21:24:08 +0545

    init  js commit

---
+++ script.js
@@ -0,0 +1,45 @@
+/*
+    GitHub : https://github.com/niirmaaltwaatii/GuessNum
+*/
+var tries = 1;
+var pre = [];
+var ran = Math.floor(Math.random()*100 + 1);
+function check() {
+    var inp = document.querySelector("#inp").value;
+    var res = document.querySelector("#result");
+    var prev = document.querySelector("#prev");
+    if(tries<10){
+        if(inp==ran){
+            tries++;
+            res.innerHTML = "Gotcha !!! You got it Right in "+tries+" tries !";
+            res.style.background = "Green";
+            res.style.color = "white";
+            inp = "";
+        }else if(inp>ran){
+            res.innerHTML = "Your Guess is too High !";
+            res.style.background = "red";
+            res.style.color = "black";
+            pre.push(inp);
+            prev.innerHTML = pre;
+            tries++;
+        }else if(inp<ran){
+            res.innerHTML = "Your Guess is too Low !";
+            res.style.background = "red";
+            res.style.color = "black";
+            pre.push(inp);
+            prev.innerHTML = pre;
+            tries++;
+        }else{
+            res.innerHTML = "Error Occured !";
+        }
+    }else{
+        res.innerHTML = " Game Over ! ";
+        document.querySelector("#again").style.visibility = "visible";
+    }
+    document.querySelector("#inp").value = "";
+}
+function again(){
+    window.location.reload();
+}
+console.log(ran);
+console.log(prev);
